cc7fcba82aa3d2e8bda6925eead3d9a04d683f83
jishnub <jishnub@users.noreply.github.com>
2021-02-17 13:18:15 +0400

    Add Centered

--- src/CenteredAxes.jl
+++ src/CenteredAxes.jl
@@ -1,5 +1,50 @@
 module CenteredAxes
+export Centered
 
-# Write your package code here.
+"""
+	Centered
+
+Given an array axis of length `N`, return `-N÷2:N÷2-1` for even `N` and `-N÷2:N÷2` for odd `N`. 
+This may be used to construct or shift an axis of an `OffsetArray`.
+
+# Examples
+```julia
+julia> A = Float64.(reshape(1:9, 3, 3))
+3×3 Array{Float64,2}:
+ 1.0  4.0  7.0
+ 2.0  5.0  8.0
+ 3.0  6.0  9.0
+
+julia> OffsetArray(A, Centered(), Centered())
+3×3 OffsetArray(::Array{Float64,2}, -1:1, -1:1) with eltype Float64 with indices -1:1×-1:1:
+ 1.0  4.0  7.0
+ 2.0  5.0  8.0
+ 3.0  6.0  9.0
+
+julia> OffsetArray(A, Centered(), :)
+3×3 OffsetArray(::Array{Float64,2}, -1:1, 1:3) with eltype Float64 with indices -1:1×1:3:
+ 1.0  4.0  7.0
+ 2.0  5.0  8.0
+ 3.0  6.0  9.0
+
+julia> A = Float64.(reshape(1:4, 2, 2))
+2×2 Array{Float64,2}:
+ 1.0  3.0
+ 2.0  4.0
+
+julia> OffsetArray(A, Centered(), Centered())
+2×2 OffsetArray(::Array{Float64,2}, -1:0, -1:0) with eltype Float64 with indices -1:0×-1:0:
+ 1.0  3.0
+ 2.0  4.0
+```
+"""
+struct Centered end
+
+@inline function Base.to_indices(A, ax::Tuple{Any, Vararg{Any}}, I::Tuple{Centered, Vararg{Any}})
+	ax1 = first(ax)
+	Lby2 = length(ax1)÷2
+	axc = -Lby2:length(ax1) - Lby2 - 1
+	(axc, to_indices(A, Base.tail(ax), Base.tail(I))...)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,60 @@
 using CenteredAxes
 using Test
+using OffsetArrays
 
 @testset "CenteredAxes.jl" begin
-    # Write your tests here.
+    @testset "Array" begin
+        A = ones(3,3)
+        I = to_indices(A, axes(A), (Centered(), :))
+        @test I == (-1:1, 1:3)
+
+        A = ones(2,2)
+        I = to_indices(A, axes(A), (Centered(), 1:2))
+        @test I == (-1:0, 1:2)
+    end
+    @testset "OffsetArrays" begin
+        @testset "to_indices" begin
+            A = ones(3:5, 3:5)
+            I = to_indices(A, axes(A), (Centered(), :))
+        	@test I == (-1:1, 3:5)
+        	I = to_indices(A, axes(A), (Centered(), Centered()))
+        	@test I == (-1:1, -1:1)
+        	I = to_indices(A, axes(A), (3:4, Centered()))
+        	@test I == (3:4, -1:1)
+
+        	A = ones(7:8, 2:3)
+        	I = to_indices(A, axes(A), (Centered(), :))
+        	@test I == (-1:0, 2:3)
+        	I = to_indices(A, axes(A), (:, Centered()))
+        	@test I == (7:8, -1:0)
+        end
+        @testset "offset" begin
+            @testset "Array" begin
+                A = ones(2, 2)
+                O = OffsetArray(A, Centered(), :)
+                @test axes(O) == (-1:0, axes(A,2))
+                O = OffsetArray(A, Centered(), Centered())
+                @test axes(O) == (-1:0, -1:0)
+
+                A = ones(3, 3)
+                O = OffsetArray(A, Centered(), :)
+                @test axes(O) == (-1:1, axes(A,2))
+                O = OffsetArray(A, Centered(), Centered())
+                @test axes(O) == (-1:1, -1:1)
+            end
+            @testset "OffsetArray" begin
+                A = ones(2:3, 2:3)
+                O = OffsetArray(A, Centered(), :)
+                @test axes(O) == (-1:0, axes(A,2))
+                O = OffsetArray(A, Centered(), Centered())
+                @test axes(O) == (-1:0, -1:0)
+
+                A = ones(2:4, 2:4)
+                O = OffsetArray(A, Centered(), :)
+                @test axes(O) == (-1:1, axes(A,2))
+                O = OffsetArray(A, Centered(), Centered())
+                @test axes(O) == (-1:1, -1:1)
+            end
+        end
+    end
 end
